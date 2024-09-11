0ce4f2de25f2d8bc336ea7a0f844537ffc61d5b7
ahambly23 <ashleyhambly4@hotmail.com>
2024-09-11 22:33:57 +0100

    Improved styling

--- script.js
+++ script.js
@@ -55,7 +55,6 @@ const plusOne = (team) => {
         guestScoreboard.innerText = guestScore;
     }
 }
-
 const plusTwo = (team) => {
     if (team === homePlusTwo) {
         homeScore += 2;
@@ -66,7 +65,6 @@ const plusTwo = (team) => {
         guestScoreboard.innerText = guestScore;
     }
 }
-
 const plusThree = (team) => {
     if (team === homePlusThree) {
         homeScore += 3;
@@ -93,192 +91,39 @@ homePlusTwo.addEventListener("click", () => {plusTwo(homePlusTwo);});
 homePlusThree.addEventListener("click", () => {plusThree(homePlusThree);});
 resetButton.addEventListener("click", reset);
 
-const teamObj = [{
-    name: "default color",
-    primary: "black",
-    secondary: "white",
-    third: "gray"
-}, 
-{
-    name: "Hawks",
-    primary: "#E03A3E",
-    secondary: "#26282A",
-    third: "#C1D32F"
-}, 
-{
-    name: "Celtics",
-    primary: "#007A33",
-    secondary: "#BA9653",
-    third: "#FFFFFF"
-}, 
-{
-    name: "Nets",
-    primary: "gray",
-    secondary: "white",
-    third: "black"
-}, 
-{
-    name: "Hornets",
-    primary: "#00788C",
-    secondary: "#1D1160",
-    third: "#A1A1A4"
-}, 
-{
-    name: "Bulls",
-    primary: "#CE1141",
-    secondary: "black",
-    third: "gray"
-}, 
-{
-    name: "Cavaliers",
-    primary: "#860038",
-    secondary: "#FDBB30",
-    third: "#041E42"
-}, 
-{
-    name: "Mavericks",
-    primary: "#B8C4CA",
-    secondary: "#002B5E",
-    third: "#00538C"
-}, 
-{
-    name: "Nuggets",
-    primary: "#0E2240",
-    secondary: "#FEC524",
-    third: "#8B2131"
-}, 
-{
-    name: "Pistons",
-    primary: "#C8102E",
-    secondary: "#1D42BA",
-    third: "#002D62"
-}, 
-{
-    name: "Warriors",
-    primary: "#FFC72C",
-    secondary: "#1D428A",
-    third: "gold"
-}, 
-{
-    name: "Rockets",
-    primary: "#CE1141",
-    secondary: "#C4CED4",
-    third: "black"
-}, 
-{
-    name: "Pacers",
-    primary: "#FDBB30",
-    secondary: "#002D62",
-    third: "#BEC0C2"
-}, 
-{
-    name: "Clippers",
-    primary: "#C8102E",
-    secondary: "#1D428A",
-    third: "#BEC0C2"
-}, 
-{
-    name: "Lakers",
-    primary: "#552583",
-    secondary: "#FDB927",
-    third: "yellow"
-}, 
-{
-    name: "Grizzlies",
-    primary: "#12173F",
-    secondary: "#5D76A9",
-    third: "#707271"
-}, 
-{
-    name: "Heat",
-    primary: "#000000",
-    secondary: "#98002E",
-    third: "#F9A01B"
-}, 
-{
-    name: "Bucks",
-    primary: "#00471B",
-    secondary: "#EEE1C6",
-    third: "#0077C0"
-}, 
-{
-    name: "Timberwolves",
-    primary: "#0C2340",
-    secondary: "#9EA2A2",
-    third: "#236192"
-}, 
-{
-    name: "Pelicans",
-    primary: "#0C2340",
-    secondary: "#C8102E",
-    third: "#85714D"
-}, 
-{
-    name: "Knicks",
-    primary: "#F58426",
-    secondary: "#006BB6",
-    third: "#BEC0C2"
-}, 
-{
-    name: "Thunder",
-    primary: "#007AC1",
-    secondary: "#FDBB30",
-    third: "#EF3B24"
-}, 
-{
-    name: "Magic",
-    primary: "#0077C0",
-    secondary: "#C4CED4",
-    third: "#000000"
-}, 
-{
-    name: "Sixers",
-    primary: "#006BB6",
-    secondary: "#C4CED4",
-    third: "#ED174C"
-}, 
-{
-    name: "Suns",
-    primary: "#1D1160",
-    secondary: "#E56020",
-    third: "#DE7F53"
-}, 
-{
-    name: "Blazers",
-    primary: "#E03A3E",
-    secondary: "black",
-    third: "gray"
-}, 
-{
-    name: "Kings",
-    primary: "#5A2D81",
-    secondary: "#000000",
-    third: "#63727A"
-}, 
-{
-    name: "Spurs",
-    primary: "black",
-    secondary: "#C4CED4",
-    third: "white"
-}, 
-{
-    name: "Raptors",
-    primary: "#CE1141",
-    secondary: "#000000",
-    third: "#B4975A"
-},  
-{
-    name: "Jazz",
-    primary: "#F9A01B",
-    secondary: "#002B5C",
-    third: "#00471B"
-}, 
-{
-    name: "Wizards",
-    primary: "#002B5C",
-    secondary: "#E31837",
-    third: "#C4CED4"
-}];
+const teamObj = [
+    {name: "default color", primary: "black", secondary: "white", third: "gray"}, 
+    {name: "Hawks", primary: "#E03A3E", secondary: "#26282A", third: "#C1D32F"}, 
+    {name: "Celtics", primary: "#007A33", secondary: "#BA9653", third: "#FFFFFF"}, 
+    {name: "Nets", primary: "gray", secondary: "white", third: "black"}, 
+    {name: "Hornets", primary: "#00788C", secondary: "#1D1160", third: "#A1A1A4"}, 
+    {name: "Bulls", primary: "#CE1141", secondary: "black", third: "gray"}, 
+    {name: "Cavaliers", primary: "#860038", secondary: "#FDBB30", third: "#041E42"}, 
+    {name: "Mavericks", primary: "#B8C4CA", secondary: "#002B5E", third: "#00538C"}, 
+    {name: "Nuggets", primary: "#0E2240", secondary: "#FEC524", third: "#8B2131"}, 
+    {name: "Pistons", primary: "#C8102E", secondary: "#1D42BA", third: "#002D62"}, 
+    {name: "Warriors", primary: "#FFC72C", secondary: "#1D428A", third: "gold"}, 
+    {name: "Rockets", primary: "#CE1141", secondary: "#C4CED4", third: "black"}, 
+    {name: "Pacers", primary: "#FDBB30", secondary: "#002D62", third: "#BEC0C2"}, 
+    {name: "Clippers", primary: "#C8102E", secondary: "#1D428A", third: "#BEC0C2"}, 
+    {name: "Lakers", primary: "#552583", secondary: "#FDB927", third: "yellow"}, 
+    {name: "Grizzlies", primary: "#12173F", secondary: "#5D76A9", third: "#707271"}, 
+    {name: "Heat", primary: "#000000", secondary: "#98002E", third: "#F9A01B"}, 
+    {name: "Bucks", primary: "#00471B", secondary: "#EEE1C6", third: "#0077C0"}, 
+    {name: "Timberwolves", primary: "#0C2340", secondary: "#9EA2A2", third: "#236192"}, 
+    {name: "Pelicans", primary: "#0C2340", secondary: "#C8102E", third: "#85714D"}, 
+    {name: "Knicks", primary: "#F58426", secondary: "#006BB6", third: "#BEC0C2"}, 
+    {name: "Thunder", primary: "#007AC1", secondary: "#FDBB30", third: "#EF3B24"}, 
+    {name: "Magic", primary: "#0077C0", secondary: "#C4CED4", third: "#000000"}, 
+    {name: "Sixers", primary: "#006BB6", secondary: "#C4CED4", third: "#ED174C"}, 
+    {name: "Suns", primary: "#1D1160", secondary: "#E56020", third: "#DE7F53"}, 
+    {name: "Blazers", primary: "#E03A3E", secondary: "black", third: "gray"}, 
+    {name: "Kings", primary: "#5A2D81", secondary: "#000000", third: "#63727A"}, 
+    {name: "Spurs", primary: "black", secondary: "#C4CED4", third: "white"}, 
+    {name: "Raptors", primary: "#CE1141", secondary: "#000000", third: "#B4975A"},  
+    {name: "Jazz", primary: "#F9A01B", secondary: "#002B5C", third: "#00471B"}, 
+    {name: "Wizards", primary: "#002B5C", secondary: "#E31837", third: "#C4CED4"}
+];
 
 function updateColorScheme(teamName) {
     const team = teamObj.find(t => t.name === teamName);
@@ -297,14 +142,26 @@ function updateColorScheme(teamName) {
             button.style.borderColor = team.secondary;
         });
         document.getElementById('reset').style.color = team.secondary;
+        document.querySelectorAll(".team-btn").forEach(el => el.style.backgroundColor = team.secondary);
+        document.querySelectorAll(".team-btn").forEach(el => el.style.borderColor = team.secondary);
 
         const styleElement = document.createElement('style');
         styleElement.innerHTML = `
-            button:active:not(#reset) {
+            .buttons button:active {
                 background-color: ${team.third} !important;
+                border-color: ${team.third} !important;
             }
             #reset:active {
                 color: ${team.third} !important;
+            }
+
+            .team-btn:active {
+                background-color: ${team.third} !important;
+                border-color: ${team.third} !important;
+            }
+
+            .team-btn {
+                color: white !important;
             }
         `;
         document.head.appendChild(styleElement);
